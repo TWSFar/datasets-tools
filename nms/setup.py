571958eb0671e32e76a70a6be23576c7ca402145
TWSFar <twsfcz@163.com>
2019-10-22 16:50:55 +0800

    update

--- nms/setup.py
+++ nms/setup.py
@@ -4,11 +4,12 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 
-extensions = [Extension("nms", ["nms.pyx"], extra_compile_args=["-Wno-cpp", "-Wno-unused-function"])]
+extensions = [Extension("nms_gpu", ["nms_gpu.pyx"], extra_compile_args=["-Wno-cpp", "-Wno-unused-function"]),
+              Extension("bbox_gpu", ["bbox_gpu.pyx"], extra_compile_args=["-Wno-cpp", "-Wno-unused-function"])]
 
 
 setup(
-    name="nms_modules",
+    name="gpu_functions",
     ext_modules=cythonize(extensions),
     include_dirs=[numpy.get_include()]
 )
