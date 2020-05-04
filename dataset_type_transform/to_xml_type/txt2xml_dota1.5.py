f3068f820507ab997ddb1e8ca2a40d37b786b3cb
TWSFar <twsfcz@163.com>
2020-05-04 23:19:56 +0800

    'update'

---
+++ dataset_type_transform/to_xml_type/txt2xml_dota1.5.py
@@ -0,0 +1,140 @@
+"""
+visdrone:
+    if we use mmdetection train and val,
+    id begin idx 1 is best. becase, in mmdetection, the default
+    classes's number is n+1, we don't know the background's idx.
+    However, in other detector, the idx begin 0 or 1 is need conside.
+
+    note:
+        classes = (plane, ship, storage-tank, baseball-diamond,
+        tennis-court, basketball-court, ground-track-field,
+        harbor, bridge, small-vehicle, large-vehicle, helicopter,
+        roundabout, soccer-ball-field, swimming-pool, container-crane)
+"""
+import os
+import cv2
+import os.path as osp
+import numpy as np
+from tqdm import tqdm
+from xml.etree.ElementTree import Element, SubElement, tostring
+from xml.dom.minidom import parseString
+import matplotlib.pyplot as plt
+
+
+hyp = {
+    'dataset': 'DOTA1.5',
+    'img_type': '.png',
+    'mode': 'train',  # for save Set: train.txt
+    'data_dir': 'G:\\CV\\Dataset\\Detection\\DOTA\\DOTA_V15\\train',
+    'show': True
+}
+hyp['xml_dir'] = osp.join(hyp['data_dir'], 'annotations_xml')
+hyp['txt_dir'] = osp.join(hyp['data_dir'], 'annotations')
+hyp['img_dir'] = osp.join(hyp['data_dir'], 'images')
+hyp['set_dir'] = osp.join(hyp['data_dir'], 'ImageSets', 'Main')
+
+
+def show_image(img, labels):
+    plt.figure(figsize=(10, 10))
+    plt.subplot().imshow(img)
+    plt.plot(labels[:, [0, 0, 2, 2, 0]].T, labels[:, [1, 3, 3,  1, 1]].T, '-')
+    # plt.savefig('test_0.jpg')
+    plt.show()
+
+
+def getGTBox_DOTA(anno_path, **kwargs):
+    box_all = []
+    gt_cls = []
+    difficult = []
+    with open(anno_path, 'r') as f:
+        for line in f.readlines():
+            data = line.split()
+            if (len(data) == 10):
+                box_all.append([float(data[0]), float(data[1]), float(data[4]), float(data[5])])
+                gt_cls.append(str(data[8].strip()))
+                difficult.append(int(data[9]))
+
+    return box_all, gt_cls, difficult
+
+
+def make_xml(box_list, label_list, difficult_list, image_name, tsize):
+    node_root = Element('annotation')
+
+    node_folder = SubElement(node_root, 'folder')
+    node_folder.text = hyp['dataset']
+
+    node_filename = SubElement(node_root, 'filename')
+    node_filename.text = image_name
+
+    node_object_num = SubElement(node_root, 'object_num')
+    node_object_num.text = str(len(box_list))
+
+    node_size = SubElement(node_root, 'size')
+    node_width = SubElement(node_size, 'width')
+    node_width.text = str(tsize[1])  # tsize: (h, w)
+    node_height = SubElement(node_size, 'height')
+    node_height.text = str(tsize[0])  # tsize: (h, w)
+    node_depth = SubElement(node_size, 'depth')
+    node_depth.text = '3'
+
+    for i in range(len(box_list)):
+        node_object = SubElement(node_root, 'object')
+        node_name = SubElement(node_object, 'name')
+        node_name.text = str(label_list[i])
+        node_difficult = SubElement(node_object, 'difficult')
+        node_difficult.text = str(difficult_list[i])
+
+        # voc dataset is 1-based
+        node_bndbox = SubElement(node_object, 'bndbox')
+        node_xmin = SubElement(node_bndbox, 'xmin')
+        node_xmin.text = str(int(box_list[i][0]) + 1)
+        node_ymin = SubElement(node_bndbox, 'ymin')
+        node_ymin.text = str(int(box_list[i][1]) + 1)
+        node_xmax = SubElement(node_bndbox, 'xmax')
+        node_xmax.text = str(int(box_list[i][2] + 1))
+        node_ymax = SubElement(node_bndbox, 'ymax')
+        node_ymax.text = str(int(box_list[i][3] + 1))
+
+    xml = tostring(node_root, encoding='utf-8')
+    dom = parseString(xml)
+    # print(xml)
+    return dom
+
+
+if __name__ == '__main__':
+    setList = []
+    if not osp.exists(hyp['xml_dir']):
+        os.makedirs(hyp['xml_dir'])
+
+    txt_files = os.listdir(hyp['txt_dir'])
+
+    for file_name in tqdm(txt_files):
+        setList.append(file_name[:-4])
+
+        anno_txt = osp.join(hyp['txt_dir'], file_name)
+        box_all, gt_cls, difficult = getGTBox_DOTA(anno_txt)
+
+        # image info
+        img_name = file_name[:-4] + hyp['img_type']  # image name
+        img_path = osp.join(hyp['img_dir'], img_name)  # image path
+        img = plt.imread(img_path)
+        tsize = img.shape[:2]
+
+        dom = make_xml(box_all, gt_cls, difficult, img_name, tsize)
+
+        # save
+        anno_xml = os.path.join(hyp['xml_dir'], file_name[:-4] + '.xml')
+        with open(anno_xml, 'w') as fx:
+            fx.write(dom.toprettyxml(indent='\t', encoding='utf-8').decode('utf-8'))
+
+        if hyp['show']:
+            show_image(img, np.array(box_all))
+
+    # save ImageSet
+    if not osp.exists(hyp['set_dir']):
+        os.makedirs(hyp['set_dir'])
+    setPath = osp.join(hyp['set_dir'], hyp['mode'] + '.txt')
+    with open(setPath, 'w') as f:
+        for line in setList:
+            f.writelines(line + '\n')
+    pass
